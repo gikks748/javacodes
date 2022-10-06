2d4b08b3364914ccf0d71ca435d20464a4221a57
gikks748 <57894529+gikks748@users.noreply.github.com>
2022-10-06 15:55:54 +0530

    salary changed

--- JBK/src/Sept22EmpSalConstructor/SalaryS.java
+++ JBK/src/Sept22EmpSalConstructor/SalaryS.java
@@ -5,7 +5,7 @@ public class SalaryS {
 
 	public static void main (String[] args){
 		Salary Emp1 = new Salary();
-		Emp1.accept("Gajanan", 102 ,"Manager", 150000);
+		Emp1.accept("Gajanan", 102 ,"Manager", 18000);
 		Emp1.accept("Ramesh",102 , "Developer", 150000);
 		Emp1.accept("Suresh",103 , "Tester", 150000);
 	}
