4d8b59099c116609dcb23c8c29f629ad2e20e322
gikks748 <57894529+gikks748@users.noreply.github.com>
2022-10-06 16:49:16 +0530

    number changed

--- JBK/src/sept28_Maths/PrimeNum.java
+++ JBK/src/sept28_Maths/PrimeNum.java
@@ -4,7 +4,7 @@ public class PrimeNum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 51;
+		int a = 511;
 		int total = 0;
 		
 		for (int i=1; i<=a; a++) {
